50c45bc27de0686d5c7b155235ed205236e9db5b
Milan Mishra <milanmishra1206@gmail.com>
2025-11-10 00:09:50 +0530

    Updating basename for GH pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,26 @@ import { AppSnackbarProvider } from "./config/Context/SnackbarContext";
 import Theme from "./config/Theme";
 import Router from "./utils/Router";
 import { SnackbarProvider } from "notistack";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 
 function App() {
   return (
-    <div>
-      <Theme>
-        <SnackbarProvider
-          maxSnack={3}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          autoHideDuration={3000}
-        >
-          <AppSnackbarProvider>
-            <Router />
-          </AppSnackbarProvider>
-        </SnackbarProvider>
-      </Theme>
-    </div>
+    <Theme>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        autoHideDuration={3000}
+      >
+        <AppSnackbarProvider>
+          <Router />
+        </AppSnackbarProvider>
+      </SnackbarProvider>
+    </Theme>
   );
 }
 
