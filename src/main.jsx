135eccfea22c80e9916df9ae57548d271048c3a8
Milan Mishra <milanmishra1206@gmail.com>
2025-11-09 15:27:47 +0530

    Adding basename

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/pure-skyn-ecom">
       <Provider store={store}>
         <App />
         </Provider>
